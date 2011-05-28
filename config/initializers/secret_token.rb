f698aad50f2ca2f6302f80efd497fc76b15eb225
charisma <charisma@charisma-Extensa-4630Z>
2011-05-28 18:15:34 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = 'b2f2ca90e7567c50d4434bd6aa1c8f66e8e2376dc57777803094323b7063b027390a75c76db32106f3dd8e639764b163aacf344e370540b4e84aaf0648a88065'
